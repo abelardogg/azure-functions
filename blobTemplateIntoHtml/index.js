726c09682dcc901f1d6526f666ca58d2dfcddfc2
abelardogg <abelardogy@gmail.com>
2022-02-28 12:21:06 -0700

    use html file as template

--- blobTemplateIntoHtml/index.js
+++ blobTemplateIntoHtml/index.js
@@ -1,24 +1,19 @@
-const doT = require('dot')
+const doT = require('dot');
+const templates =  require('./modules/template_reader.js');
 
 module.exports = async function(context) {
+
     const fileInBlob = context.bindings.inputBlob;
     const data = JSON.parse(fileInBlob);
 
     context.log('Node.js Blob trigger function processed', data);
 
-    let template = doT.template(presentationCardTemplate());
-    const result = template(data);
+    const HTMLtemplate = await templates.getTemplate('user-card');
+    let doTTemplate = doT.template(HTMLtemplate);
+    const result = doTTemplate(data);
 
 
     context.bindings.outputBlob = result;
 };
 
 
-function presentationCardTemplate(){
-    return `
-        <h1>Name: {{=it.name}}</h1>
-        <h3>Age: {{=it.age}}</h3>
-        <h3>Role: {{=it.role}}</h3>
-        <h3>Location: {{=it.location}}</h3>
-    `;
-}
